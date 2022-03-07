8bf511ab917555dcb28c299feb6b5b0829b55bd5
Jackie Lai <jackie12051995@gmail.com>
2022-03-07 23:32:08 +1300

    Initial Commit

---
+++ ScannerApp/ScannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerAppApp.swift
+//  ScannerApp
+//
+//  Created by Jackie Tin Lok Lai on 7/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
