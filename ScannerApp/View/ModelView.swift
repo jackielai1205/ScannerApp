643fec19bf000c190498781cd2a3a85eba721e69
Walter Siu <typist95>
2022-03-19 15:20:32 +1300

    updated LoadingView, ModelView & MenuView

--- ScannerApp/View/ModelView.swift
+++ ScannerApp/View/ModelView.swift
@@ -11,15 +11,65 @@ import SceneKit
 struct ModelView: View {
     
     @State var dragon = Model(id: 1, modelName: "dragon.usdz")
-    @State var chair = Model(id: 0, modelName: "chair.usdz")
+    @State var fileName = ""
+    @State var openFile = false
+    @State var model = Model(id: 0, modelName: "")
+    @State var fileURL = ""
+    @State var view = SCNScene(named: "chair.usdz")
+    @State private var textInFiles = [String]()
     
     var body: some View {
-        
-        VStack{
-            SceneView(scene: SCNScene(named: dragon.modelName) , options: [.autoenablesDefaultLighting,.allowsCameraControl])
-                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2)
-            Spacer();
+        ScrollView {
+            ZStack{
+                Color("Background")
+                    .ignoresSafeArea(.all)
+                VStack{
+                    TopLogoBar()
+                    VStack {}
+                    .fileImporter(isPresented: $openFile, allowedContentTypes: [.usdz]) {  result in
+                        switch result {
+                        case .success(let url):
+                            _ = url.startAccessingSecurityScopedResource()
+                            print(url)
+                            fileURL = url.absoluteString
+                            self.fileName = url.lastPathComponent
+                            print(fileName)
+                            model.modelName = fileName
+                            do{
+                                try
+                                view = SCNScene(url: url, options: .none)
+                            }catch{
+                                print(error)
+                            }
+                        case.failure(let error):
+                            print(error)
+                        }
+                    }
+                    SceneView(scene: view , options: [.autoenablesDefaultLighting,.allowsCameraControl])
+                                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2)
+                    VStack {
+                        Button {
+                            openFile.toggle()
+                        } label: {
+                            Circle()
+                                .strokeBorder(Color.black, lineWidth: 2)
+                                .background(Circle().foregroundColor(Color(red: 66/255, green: 180/255, blue: 157/255)))
+                                .frame(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.height / 2.5, alignment: .top)
+                                .overlay(Text("Select Model")
+                                            .foregroundColor(Color.white)
+                                            .fontWeight(.bold))
+                                .offset(y: -(UIScreen.main.bounds.height / 9))
+                        }
+                    }
+                }
+            }
         }
+    }
+}
+
+extension URL {
+    init(_ string: StaticString) {
+        self.init(string: "\(string)")!
     }
 }
 
